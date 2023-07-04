ceab57073721942c24c296e12ab7fb52e4145363
alexbobrob <bobrov.3alex@gmail.com>
2023-07-04 10:39:41 -0400

    feat: created dummy profile

---
+++ src/components/profile/index.tsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import expandIcon from "../../assets/svgs/expandIcon.svg";
+import closeIcon from "../../assets/svgs/closeIcon.svg";
+import locationIcon from "../../assets/svgs/locationIcon.svg";
+import addFriendIcon from "../../assets/svgs/addFriendIcon.svg";
+import editIcon from "../../assets/svgs/editIcon.svg";
+import heartIcon from "../../assets/svgs/heartIcon.svg";
+
+const Profile = () => {
+  const [activeTab, setActiveTab] = useState("posts");
+  const user = {
+    picture: "",
+    name: "Terry Hamlet",
+  };
+
+  const renderProfilePicture = (dimentions: number) => {
+    if (user.picture) {
+      return (
+        <img
+          src={user.picture}
+          alt="Profile"
+          width={dimentions}
+          height={dimentions}
+          className="rounded-full"
+        />
+      );
+    } else {
+      const initials = user.name
+        .split(" ")
+        .map((word) => word[0])
+        .join("");
+      return (
+        <div
+          className="rounded-full bg-gray-500 flex items-center justify-center"
+          style={{ width: dimentions, height: dimentions }}
+        >
+          {initials}
+        </div>
+      );
+    }
+  };
+
+  const renderTabs = () => {
+    return (
+      <div className="flex justify-center w-[42%] mt-8 p-1 border border-cs-gray border-r-0 border-l-0">
+        <p
+          className="text-[12px] cursor-pointer"
+          style={{ color: activeTab === "posts" ? "#F47D00" : "" }}
+          onClick={() => setActiveTab("posts")}
+        >
+          POSTS
+        </p>
+        <p
+          className="text-[12px] mx-3 px-3 border border-t-0 border-b-0 cursor-pointer"
+          style={{ color: activeTab === "threads" ? "#F47D00" : "" }}
+          onClick={() => setActiveTab("threads")}
+        >
+          THREADS
+        </p>
+        <p
+          className="text-[12px] cursor-pointer"
+          style={{ color: activeTab === "tags" ? "#F47D00" : "" }}
+          onClick={() => setActiveTab("tags")}
+        >
+          TAGS
+        </p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="w-full h-[351px] border border-cs-gray border-[10px] p-4 text-white">
+      <div className="flex justify-between">
+        <div className="flex w-[50%] justify-between">
+          <div className="flex flex-col items-center">
+            {renderProfilePicture(45)}
+            <div className="flex items-center justify-between mt-1">
+              <div className="rounded-full w-[10px] h-[10px] bg-cs-green flex items-center justify-center"></div>
+              <div className="ml-1 text-[12px]">Available</div>
+            </div>
+          </div>
+          <div className="pr-[100px] pl-10">
+            <div className="flex items-center">
+              <p className="font-bold text-[14px] text-cs-flushed">
+                {user.name}
+              </p>
+              <div className="mx-2">
+                <img src={addFriendIcon} alt="Add Friend" className="cursor-pointer"/>
+              </div>
+              <div>
+                <img src={editIcon} alt="Edit" className="cursor-pointer"/>
+              </div>
+            </div>
+            <div className="text-[12px]">SVP, Property Fac Broker Aon Re</div>
+          </div>
+        </div>
+        <div className="flex w-[50%] justify-between">
+          <div className="flex mt-1">
+            <div className="text-[11px] py-[1px] px-6 h-fit bg-cs-gray flex flex-col items-center">
+              <p>FOLLOWERS</p>
+              <p className="text-cs-light-orange">0</p>
+            </div>
+            <div className="text-[11px] py-[1px] px-6 h-fit bg-cs-gray flex flex-col items-center ml-3">
+              <p>FOLLOWING</p>
+              <p className="text-cs-light-orange">0</p>
+            </div>
+          </div>
+          <div className="flex h-[20px]">
+            <img src={locationIcon} alt="Add Friend" className="cursor-pointer"/>
+            <img src={expandIcon} alt="Add Friend" className="cursor-pointer"/>
+            <img src={closeIcon} alt="Add Friend" className="cursor-pointer"/>
+          </div>
+        </div>
+      </div>
+      {renderTabs()}
+      <div className="flex mt-8 w-[80%]">
+        <div className="mr-2 mt-[-8px]">{renderProfilePicture(35)}</div>
+        <div className="mr-8">
+          <p className="font-bold text-[14px] text-cs-flushed">{user.name}</p>
+          <p className="text-[14px] ml-2">
+            Hello World! This is my first ever post on the fuse platform! Happy
+            to be here!
+          </p>
+          <div className="flex mt-1">
+            <img src={heartIcon} alt="Add Friend" className="mr-1 cursor-pointer"/>
+            <img src={closeIcon} alt="Add Friend" className="cursor-pointer"/>
+          </div>
+        </div>
+        <div className="font-bold text-[10px] text-cs-gray">20m</div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Profile from '../components/profile'
 
 const Home = () => {
   return (
-    <div>Welcome Alex</div>
+    <div className='bg-cs-body-gray h-screen py-[5%] text-white'>
+      <div className='bg-cs-dark-gray mx-[2%] h-full'>
+        <div className='mx-[15%] py-[8%]'>
+          <div className='flex'>
+            <div className='w-[20%] bg-cs-gray mr-2'>
+            </div>
+            <div className='w-[80%]'>
+              <div className='flex justify-end mb-4 bg-cs-gray h-10'>
+              </div>
+              <Profile />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
